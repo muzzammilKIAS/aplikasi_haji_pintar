d523798e9547116d3da621627efc20bad4f83dd0
Muzzammil Najib <sufyanthawry@Muzzammils-MacBook-Air.local>
2026-07-27 20:42:59 +0800

    Tukar nama dan ikon aplikasi kepada hajipintar

---
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,14 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import audioplayers_darwin
+import printing
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
+  PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
+}
